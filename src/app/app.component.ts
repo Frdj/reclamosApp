af5cb4428886d3680bb06505d82b2c2244c25108
Franco <fviotti@uade.edu.ar>
2019-08-18 17:19:24 -0300

    agregado de angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'reclamosApp';
+  public router: Router;
+
+  constructor(router: Router){
+    this.router = router;
+  }
 }
