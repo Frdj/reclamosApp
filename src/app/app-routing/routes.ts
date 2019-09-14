0c391d3eecfc58a26ee5fc49bcc9e323eb0d2e3d
nicoragusa <44321207+nicoragusa@users.noreply.github.com>
2019-09-13 21:04:22 -0300

    rec

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,9 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from '../components/home/home.component';
 import { ReclamosComponent } from '../components/reclamos/reclamos.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'reclamos', component: ReclamosComponent},
+  { path: 'home', component: ReclamosComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
