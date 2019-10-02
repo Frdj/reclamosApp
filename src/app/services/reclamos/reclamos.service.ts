2b5a3ebd709f03566c290b715efa1c233f4ee343
Santiago, Mamani <santiagojmamani@gmail.com>
2019-10-02 19:15:57 -0300

    url environment

--- src/app/services/reclamos/reclamos.service.ts
+++ src/app/services/reclamos/reclamos.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 import { Reclamo } from '../../models/reclamo';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReclamosService {
+
+  url: string = environment.url;
 
   reclamo: Reclamo;
   private reclamoSource = new BehaviorSubject(this.reclamo);
@@ -15,11 +18,11 @@ export class ReclamosService {
   constructor(private httpClient: HttpClient) { }
 
   getReclamos() {
-    return this.httpClient.get('http://localhost:3000/reclamo');
+    return this.httpClient.get(this.url);
   }
 
   saveReclamo(reclamo) {
-    return this.httpClient.post('http://localhost:3000/reclamo', reclamo);
+    return this.httpClient.post(this.url, reclamo);
   }
 
   changeReclamo(reclamo: Reclamo) {
@@ -27,10 +30,10 @@ export class ReclamosService {
   }
 
   update(id: number, reclamo: Reclamo) {
-    return this.httpClient.put(`http://localhost:3000/reclamo/${id}`, reclamo);
+    return this.httpClient.put(`${this.url}/${id}`, reclamo);
   }
 
   delete(id: number) {
-    return this.httpClient.delete(`http://localhost:3000/reclamo/${id}`);
+    return this.httpClient.delete(`${this.url}/${id}`);
   }
 }
